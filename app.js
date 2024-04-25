dcfef02877ed05b6de04756fedcba80968ddcdfb
Amogh Krishna <amoghkrishna55@gmail.com>
2024-04-25 18:40:15 +0530

    Fixed Vercel Deployment

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var expressSession = require('express-session');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dataRouter = require('./routes/data');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var expressSession = require("express-session");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dataRouter = require("./routes/data");
 
-const passport = require('passport');
-const MongoStore = require('connect-mongo');
+const passport = require("passport");
+const MongoStore = require("connect-mongo");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(expressSession({
-  resave: false,
-  saveUninitialized: false,
-  secret: "hello hello",
-  cookie: { maxAge: 24 * 60 * 60 * 1000 },
-  store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/demodb' })
-}));
+app.use(
+  expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: "hello hello",
+    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/demodb" }),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.serializeUser(usersRouter.serializeUser());
 passport.deserializeUser(usersRouter.deserializeUser());
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/', dataRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/", dataRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Express server listening on port ${app.get("port")}`);
+});
