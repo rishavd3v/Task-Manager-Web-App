5b099776d610b704e9e30969319ac0ce6b153e8a
Rishav <rishavraj4273@gmail.com>
2024-04-28 15:36:13 +0530

    modified

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -4,8 +4,9 @@ window.onload = function(){
     activeNav.classList.add('active');
 
     //finction to display currecnt date
-    getCurrentDate();
+    setInterval(getCurrentDate);
     displayWeather();
+    displayCalendar();
 }
 
 
@@ -18,8 +19,24 @@ function getCurrentDate() {
     const dd = String(date.getDate()).padStart(2, '0');
     const mm = monthNames[date.getMonth() + 1];
     const yyyy = date.getFullYear();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
     let dateDiv = document.getElementById('date');
+    let timeDiv = document.getElementById('time');
+    let greetDiv = document.getElementById('greet');
     dateDiv.textContent = `${day} ${dd} ${mm} ${yyyy}`;
+    timeDiv.textContent = `${hour}:${min}:${sec}`;
+    if(hour >= 0 && hour < 12){
+        greetDiv.textContent = 'Morning';
+    }else if(hour==12){
+        greetDiv.textContent = 'Noon';
+    }
+    else if(hour>=12 && hour < 16){
+        greetDiv.textContent = 'Afternoon';
+    }
+    else greetDiv.textContent = 'Evening';
 }
 
 
@@ -35,9 +52,10 @@ async function displayWeather() {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
     const weatherData = await response.json();
+    console.log(weatherData);
     const curTemp = weatherData.main.temp; // temp in C.
     const location = weatherData.name;
-    const cloud = weatherData.weather[0].main;
+    const cloud = weatherData.weather[0].description;
 
     const locDiv = document.getElementById("location");
     const tempDiv = document.getElementById('curTemp');
@@ -48,11 +66,11 @@ async function displayWeather() {
     cloudDiv.textContent = cloud;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+function displayCalendar() {
     const calendarEl = document.getElementById('calendar')
     const calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
       selectable: true,
     })
     calendar.render()
-})
+}
